77b659e9444a395929b2ccfcfb959adaa212cf37
ch-tyson <cheungtyson@gmail.com>
2023-01-05 16:57:18 -0500

    Bot setup with ShardManager

---
+++ src/main/java/Geisha.java
@@ -0,0 +1,49 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.security.auth.login.LoginException;
+
+public class Geisha {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    /**
+     * Loads environment variables and builds the bot shard manager.
+     * @throws LoginException when the bot token is invalid.
+     */
+    public Geisha() throws LoginException {
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.watching("tyson cry"));
+
+        //Enables intents for guild members, messages, and user presence
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS);
+
+        //Setting up flags for user cache on Geisha load-up
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setChunkingFilter(ChunkingFilter.ALL);
+        builder.enableCache(CacheFlag.ONLINE_STATUS);
+
+
+        shardManager = builder.build();
+    }
+
+        public static void main(String[] args) {
+        try {
+            Geisha bot = new Geisha();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Provided bot token is invalid.");
+        }
+    }
+}
